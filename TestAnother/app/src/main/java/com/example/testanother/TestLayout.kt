26bc0666371fac5ec8a304d0d0578b29df07483e
Aquavirus <rjismail202@gmail.com>
2020-01-15 08:02:19 +0100

    This is the first commit after learning how to use Room

---
+++ TestAnother/app/src/main/java/com/example/testanother/TestLayout.kt
@@ -0,0 +1,20 @@
+package com.example.tesstapp.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.example.testanother.R
+
+class GalleryFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ):
+}
